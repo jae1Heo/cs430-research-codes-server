08b3ec050fe038953448006f931c3efdae43c4ee
jae1Heo <wodnjs7791@gmail.com>
2025-11-29 16:24:26 -0800

    auth.h openssl libraries included

--- auth/auth.h
+++ auth/auth.h
@@ -1,6 +1,12 @@
 #ifndef __AUTH_H__
 #define __AUTH_H__
 
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/sha.h>
+// -lssl -lcrypto required
+// sudo apt install libssl-dev
+// create a Makefile for this
 
 
 #endif
--- client_mgmt/clnt.h
+++ client_mgmt/clnt.h
@@ -1,6 +1,10 @@
 #ifndef __CLNT_H__
 #define __CLNT_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 
 #endif
