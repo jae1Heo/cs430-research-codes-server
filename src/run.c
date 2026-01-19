ccaa1e375bfe6d75eae9d33479f478fc4de00b00
rost <beef@fwefwer.erge.revger.bre>
2026-01-18 21:53:13 -0800

    minor change

--- src/run.c
+++ src/run.c
@@ -91,7 +91,7 @@ int main(int argc, char* argv[]) {
                 reset(&g_data);
                 // send client initial data 
                 // required = left_x, left_y, right_x, right_y, ball_x, ball_y, score_left, score_right, ball_vel_x, ball_vel_y
-
+                
                 g_data.game_status = 1;
             }
         }
